ff5fedbbd46244f793b00fa7916ea1694983428f
saksjain <saksjain@adobe.com>
2021-01-18 15:11:52 +0530

    employee operations

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/EmployeeService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/EmployeeService.java
@@ -1,2 +1,16 @@
-package com.udacity.jdnd.course3.critter.services;public class EmployeeService {
+package com.udacity.jdnd.course3.critter.services;
+
+import com.udacity.jdnd.course3.critter.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+
+
 }
